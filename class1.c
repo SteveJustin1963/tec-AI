732c9f5eb6dfc60b4a20999e62b04206eefe8474
Stephen Justin <stevejustin1963@gmail.com>
2023-01-21 20:09:21 +1100

    Create class1.c

---
+++ class1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct data {
+  int age;
+  char gender;
+  char class;
+};
+
+int main() {
+  struct data input;
+  printf("Enter the age: ");
+  scanf("%d", &input.age);
+  printf("Enter the gender (M/F): ");
+  scanf(" %c", &input.gender);
+
+  if (input.age < 18) {
+    input.class = 'C';
+  } else if (input.gender == 'M') {
+    input.class = 'A';
+  } else {
+    input.class = 'B';
+  }
+
+  printf("The class is: %c\n", input.class);
+  return 0;
+}

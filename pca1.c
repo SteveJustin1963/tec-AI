af55816ee2227ea7f91b7a4e155ba3f9f3c66f24
Stephen Justin <stevejustin1963@gmail.com>
2023-06-21 10:52:06 +1000

    Update pca1.c

--- pca1.c
+++ pca1.c
@@ -7,14 +7,28 @@ using namespace arma;
 int main() {
     int n_samples = 1000;
     int n_features = 10;
-    mat X(n_samples, n_features);
-    // Fill X with data
-    mat centered = X - mean(X);
+
+    // Initialize a matrix X with random values
+    mat X = randu<mat>(n_samples, n_features);
+
+    // Center the data by subtracting the mean of each column
+    mat centered = X - repmat(mean(X, 0), n_samples, 1);
+
+    // Calculate the covariance matrix
     mat cov_mat = cov(centered);
+
+    // Perform the eigendecomposition
     vec eigval;
     mat eigvec;
-    eig_sym(eigval, eigvec, cov_mat);
+    if(!eig_sym(eigval, eigvec, cov_mat)) {
+        cerr << "Eigendecomposition failed!" << endl;
+        return -1;
+    }
+
+    // Project data onto the last two principal components
     mat pca = centered * eigvec.cols(n_features - 2, n_features - 1);
-    // Use pca for further processing
+
+    // TODO: Use pca for further processing
+
     return 0;
 }

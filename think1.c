3e66199288af364dee62cea3b3677525ed32b2c0
Stephen Justin <stevejustin1963@gmail.com>
2023-06-21 10:57:03 +1000

    Update think1.c

--- think1.c
+++ think1.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
+int cube(int number) {
+    return number * number * number;
+}
+
+int square(int number) {
+    return number * number;
+}
+
 int main() {
-  int i, sum_of_cubes = 0, sum_of_squares = 0;
+    int i;
+    int sum_of_cubes = 0;
+    int sum_of_squares = 0;
 
-  // iterate over the numbers 1 to 10
-  for (i = 1; i <= 10; i++) {
-    sum_of_cubes += i * i * i;   // calculate the cube of the current number and add it to the sum of cubes
-    sum_of_squares += i * i;     // calculate the square of the current number and add it to the sum of squares
-  }
+    for (i = 1; i <= 10; i++) {
+        sum_of_cubes += cube(i);
+        sum_of_squares += square(i);
+    }
 
-  // print the sum of cubes
-  printf("The sum of cubes of numbers from 1 to 10 is: %d\n", sum_of_cubes);
-  // print the sum of squares
-  printf("The sum of squares of numbers from 1 to 10 is: %d\n", sum_of_squares);
+    printf("The sum of cubes of numbers from 1 to 10 is: %d\n", sum_of_cubes);
+    printf("The sum of squares of numbers from 1 to 10 is: %d\n", sum_of_squares);
 
-  return 0;
+    return 0;
 }

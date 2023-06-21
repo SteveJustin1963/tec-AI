784e43de738eb3a7fc6f762d8edcc7bc5ea8b576
Stephen Justin <stevejustin1963@gmail.com>
2023-06-21 10:49:32 +1000

    Update gb1.c

--- gb1.c
+++ gb1.c
@@ -6,30 +6,42 @@ int main() {
     int i;
     int n_samples = 1000;
     int n_features = 10;
-    int n_trees = 100;
+    int n_rounds = 10;
     float *X = (float *)malloc(n_samples * n_features * sizeof(float));
     float *y = (float *)malloc(n_samples * sizeof(float));
-    // Fill X and y with data
+
+    // TODO: Fill X and y with data
+
     DMatrixHandle h_train;
-    XGDMatrixCreateFromMat(X, n_samples, n_features, -1, &h_train);
+    XGDMatrixCreateFromMat((float *)X, n_samples, n_features, -1, &h_train);
     XGDMatrixSetFloatInfo(h_train, "label", y, n_samples);
+
     BoosterHandle h_booster;
-    XGBoosterCreate(h_train, 0, &h_booster);
+    XGBoosterCreate(NULL, 0, &h_booster);
     XGBoosterSetParam(h_booster, "objective", "reg:linear");
     XGBoosterSetParam(h_booster, "eval_metric", "rmse");
     XGBoosterSetParam(h_booster, "eta", "0.1");
     XGBoosterSetParam(h_booster, "max_depth", "3");
     XGBoosterSetParam(h_booster, "subsample", "0.5");
     XGBoosterSetParam(h_booster, "colsample_bytree", "0.5");
-    XGBoosterSetParam(h_booster, "num_round", "10");
-    XGBoosterUpdateOneIter(h_booster, 0, h_train);
+
+    for (i = 0; i < n_rounds; i++) {
+        XGBoosterUpdateOneIter(h_booster, i, h_train);
+    }
+
     bst_ulong out_len;
     const float *f;
     XGBoosterPredict(h_booster, h_train, 0, 0, &out_len, &f);
-    // Use f for further processing
-    XGDMatrixfree(h_train);
-XGBoosterFree(h_booster);
-free(X);
-free(y);
-return 0;
+
+    // TODO: Use f for further processing
+
+    XGBoosterFree(h_booster);
+    XGDMatrixFree(h_train);
+    free(X);
+    free(y);
+
+    return 0;
 }
+; remember to replace // TODO: Fill X and y with data with the appropriate code to fill 
+;your X and y arrays with training data. Also replace // TODO: Use f for further processing 
+;with the code to process the prediction results.
